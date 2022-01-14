ef7b558010703987875cf4c698a6f3ad71ff0b3e
pomeranian91 <forking91@gmail.com>
2022-01-14 15:25:37 +0900

    firstCommit

---
+++ src/routes/Home.js
@@ -0,0 +1,6 @@
+import "../App.css";
+
+function Home() {
+  return <img src="https://static.wanted.co.kr/images/banners/1436/e2dd9445.jpg" />;
+}
+export default Home;
---
+++ src/routes/Navbar.js
@@ -0,0 +1,6 @@
+import "../App.css";
+
+function Navbar() {
+  return <div className="test">nav</div>;
+}
+export default Navbar;
