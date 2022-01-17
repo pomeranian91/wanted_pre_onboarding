ece3a4ced0229a43a75d0f64bef3cfff49f0a31a
pomeranian91 <forking91@gmail.com>
2022-01-17 18:08:07 +0900

    slidePartOneclear

--- src/routes/Slide.js
+++ src/routes/Slide.js
@@ -6,10 +6,12 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function Slide() {
   const [SlideComposition, setSlideComposition] = useState([
-    { id: 8, img: "https://static.wanted.co.kr/images/banners/1489/312a0c29.jpg", title: "2022년 달라지는 노동법령", text: "노무쟁점 한 눈에 파악하기" },
     { id: 0, img: "https://static.wanted.co.kr/images/banners/1468/3df61cbc.jpg", title: "해, 커리어 EP 02 공개", text: "마지막 관문 2라운드의 승자는?" },
     { id: 1, img: "https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg", title: "개발자 되고싶은 분들!?", text: "프론트엔드 무료 교육과정 참여하기" },
     { id: 2, img: "https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg", title: "개발자 성장 비결 공개", text: "Velog, 글 쓰는 개발자들의 이야기" },
@@ -17,54 +19,38 @@ function Slide() {
     { id: 4, img: "https://static.wanted.co.kr/images/banners/1486/fba2df30.jpg", title: "성과를 내는 마케팅", text: "실제 사례를 공개합니다" },
     { id: 5, img: "https://static.wanted.co.kr/images/banners/1487/0d36f0b5.jpg", title: "포트폴리오를 부탁해!", text: "디자이너의 포폴 살펴보기" },
     { id: 6, img: "https://static.wanted.co.kr/images/banners/1489/312a0c29.jpg", title: "2022년 달라지는 노동법령", text: "노무쟁점 한 눈에 파악하기" },
-    { id: 7, img: "https://static.wanted.co.kr/images/banners/1468/3df61cbc.jpg", title: "해, 커리어 EP 02 공개", text: "마지막 관문 2라운드의 승자는?" },
   ]);
-  const [addClass, setAddClass] = useState(true);
-  const TOTAL_SLIDES = 5;
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const slideRef = useRef(null);
 
-  const nextSlide = () => {
-    if (currentSlide >= TOTAL_SLIDES) {
-      setCurrentSlide(-4);
-    } else {
-      setCurrentSlide(currentSlide + 1);
-    }
-    console.log({ currentSlide });
+  const settings = {
+    dots: false,
+    infinite: true,
+    adaptiveHeight: true,
+    speed: 1000,
+    arrows: false,
+    slidesToShow: 1.75,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
-  const prevSlide = () => {
-    if (currentSlide === -4) {
-      setCurrentSlide(TOTAL_SLIDES);
-    } else {
-      setCurrentSlide(currentSlide - 1);
-    }
-    console.log({ currentSlide });
-  };
-  useEffect(() => {
-    slideRef.current.style.transition = "all 0.5s ease-in-out";
-    slideRef.current.style.transform = `translateX(calc(${currentSlide}*-1090px))`;
-  }, [currentSlide]);
-  // // 자동슬라이드
-  // const autoSlide = () => {
-  //   if (1 === 1) {
-  //     slideRef.current.style.transform = `translateX(calc(${currentSlide}*-1090px)`;
-  //   } else {
-  //     clearInterval(autoSlide);
-  //   }
-  // };
-  // useEffect(() => {
-  //   setInterval(autoSlide, 4000);
-  //   return () => clearInterval(autoSlide);
-  // }, []);
-
   return (
-    <div className="Slide_container">
-      <div className="Slide_list" ref={slideRef}>
-        {SlideComposition.map((a, i) => {
+    <div className="test">
+      <Slider {...settings}>
+        {SlideComposition.map((a) => {
           return (
-            <div className="Slide_detail" kye={a} index={i}>
-              <img className="Slide_detail_img" src={a.img} alt="슬라이드이미지" />
-              <div className={`Slide_detail_text ${addClass === true ? "Slide_center" : "Slide_text_side"}`}>
+            <div className="Slide_container" data={SlideComposition} handleRen={setSlideComposition}>
+              <div className="Slide_detail">
+                <img className="Slide_detail_img" src={a.img} />
+              </div>
+              <div className="Slide_detail_text">
                 <h2>{a.title}</h2>
                 <h3>{a.text}</h3>
                 <hr />
@@ -78,15 +64,21 @@ function Slide() {
             </div>
           );
         })}
-      </div>
-      <button className="btn_left">
-        <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide} />
-      </button>
-      <button className="btn_right">
-        <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide} />
-      </button>
+      </Slider>
     </div>
   );
 }
 
 export default Slide;
+
+{
+  /* <button className="btn_left ">
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+        <button className="btn_right ">
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button> */
+}
+{
+  /*클래스 사이 추가 ${addClass === true ? "Slide_center" : "Slide_text_side"} */
+}
